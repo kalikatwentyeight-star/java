a50514b956b3b45ccce68a185550326b0cea4756
kalikatwentyeight-star <kalikatwentyeight@gmail.com>
2025-12-01 14:47:23 +0545

    First commit

---
+++ string1by.java
@@ -0,0 +1,13 @@
+public class string1by {
+    public static void main(String[] args) {
+
+        String name = "kalika";
+        char[] arr = name.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+}
+
